6781eeb1e9f3ab0c9979feff78ba4564338388ee
Arul prasad frontend <arul.prasad@doodleblue.com>
2022-10-09 10:40:20 +0530

    remove token

--- service/index.js
+++ service/index.js
@@ -9,7 +9,6 @@ export const request = ({url, method, data, params}) =>
       params: params ?? null,
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjMzZmNhZWZlZDBkNWM1OTQxNTI1ZWUwIiwidHlwZSI6IlNUQUZGIiwiaWF0IjoxNjY1MjM2NzM2fQ.324ca0NP5Puu6axq56MJuZWYIRViE_TG7NnP_9NiY-I ',
         'Content-Type': 'application/json',
       },
     };
